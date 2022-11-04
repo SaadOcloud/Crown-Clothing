32cb100f6a4b4990971f0698626f15adb0d8bb1b
Saad Fareed <113437351+SaadOcloud@users.noreply.github.com>
2022-11-04 15:54:38 +0500

    React Routing Folder completed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import Category from './components/category/category.component';
-function App() {
-  const categories =
-    [
-      {
-        "id": 1,
-        "title": "Hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "Jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "Sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "Womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "Mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      }
-    ];
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+const Shop = () => {
   return (
-    <Category categories={categories} />
+    <div>
+      <h1>Shop</h1>
+    </div>
+  );
+};
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
-
 export default App;
-
-
